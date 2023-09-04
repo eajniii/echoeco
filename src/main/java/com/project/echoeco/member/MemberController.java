d9d96eb7aa5149ea0f1ef581ce82c5f887d393b2
jisu kim <yyjisu@gmail.com>
2023-09-05 01:35:03 +0900

    회원가입 유효성 검사 기능 ( 암호화 제외 )

--- src/main/java/com/project/echoeco/member/MemberController.java
+++ src/main/java/com/project/echoeco/member/MemberController.java
@@ -1,21 +1,46 @@
 package com.project.echoeco.member;
 
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
 
-	private MemberService memberservice;
+	private MemberService memberService;
 	
-	@ResponseBody
-	@GetMapping("/member/signup")
+	@GetMapping("/signup")
 	private String signup(MemberDTO memberDTO) {
 		return "/signup";
+	}
+
+	
+	//비밀번호 유효성 검사 실패
+	private String failsignup(@Valid MemberDTO memberDTO,Errors errors,Model model) {
+		//회원가입 실패 시 , 입력 데이터 유지 (프론트단에서 thymleaf 사용)
+		if(errors.hasErrors()) {
+			model.addAttribute("memberDTO",memberDTO);
+			
+		//유효성 통과 못한 필드 필드와 메세지 핸들링 - 예외 처리 (패스워드 유효성 검사)
+		Map<String,String> validatorResult = memberservice.validatehandling(errors);
+		for(String key:validatorResult.keySet()) {
+			model.addAttribute(key,validatorResult.get(key));
+		}
+			
+			
+		}
 	}
 	
 }
--- src/main/java/com/project/echoeco/member/MemberService.java
+++ src/main/java/com/project/echoeco/member/MemberService.java
@@ -1,8 +1,45 @@
 package com.project.echoeco.member;
 
+import java.util.Map;
+
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@Transactional
+@RequiredArgsConstructor	
 public class MemberService {
+	private final MemberRepository memberRepository;
+	
+	//회원 정보 저장
+	private Member saveMember(Member member) {
+		validateDuplicateMember(member);
+		return memberRepository.save(member);
+	}
+	
+	//중복성 검사 , email 중복인 경우 오류 메세지
+	private void validateDuplicateMember(Member member) {
+		Member findMember = memberRepository.findbyEmail(member.getEmail());
+		if(findMember != null) {
+			throw new IllegalStateException("이미 가입된 회원입니다.");
+		}
+	}
+	
+	
+	@Transactional(readOnly = true)
+	public Map<String,String> validatehandling(Errors errors){
+		Map<String,String> validatorResult = new hashMap<>();
+	
+		//유효성 검사에 실패한 필드 목록
+		
+		
+	return validatorResult;
+	}
+	
 	
 }
