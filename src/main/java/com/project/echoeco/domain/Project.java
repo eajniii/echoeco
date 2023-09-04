b29767db45b1054a5112137d237f8e37eb6d412c
HWANG INJAE <sub.ij002@gmail.com>
2023-09-05 00:25:00 +0900

    project creat ING

--- src/main/java/com/project/echoeco/domain/Project.java
+++ src/main/java/com/project/echoeco/domain/Project.java
@@ -2,6 +2,8 @@ package com.project.echoeco.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -27,14 +29,18 @@ public class Project {
 
   private String img_url;
 
-  private long goal;
+  private Integer goal;
+
+  @Enumerated(EnumType.STRING)
+  private ProjectStatus project_status;
 
   @Builder
-  public Project(String title, String content, String img_url, long goal) {
+  public Project(String title, String content, String img_url, Integer goal, ProjectStatus project_status) {
     this.title = title;
     this.content = content;
     this.img_url = img_url;
     this.goal = goal;
+    this.project_status = project_status;
   }
 
 }
