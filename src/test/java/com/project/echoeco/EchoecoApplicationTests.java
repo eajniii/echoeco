879e5364fa8d1834200dc6924c0de1ce58ab17dd
jisu kim <yyjisu@gmail.com>
2023-09-05 21:05:32 +0900

    테스트 코드 작성

--- src/test/java/com/project/echoeco/EchoecoApplicationTests.java
+++ src/test/java/com/project/echoeco/EchoecoApplicationTests.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.project.echoeco.member.Member;
 import com.project.echoeco.member.MemberRepository;
 
+import common.Role;
+
 @SpringBootTest
 class EchoecoApplicationTests {
 
@@ -16,9 +18,13 @@ class EchoecoApplicationTests {
 	@Test
 	void testJpa() {
 		Member m1 = new Member();
+		
 		m1.setEmail("123@123.com");
 		m1.setName("패트");
 		m1.setPassword("1234");
+		m1.setRole(Role.ADMIN);
+		m1.setTel(12345678);
+		this.memberRepository.save(m1);
 	}
 
 }
