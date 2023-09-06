28d46e62ed9e52f7e5fb85784c9a5e50c78cb69a
HWANG INJAE <sub.ij002@gmail.com>
2023-09-06 11:44:40 +0900

    0906 1차 병합

--- src/main/java/com/project/echoeco/project/ProjectController.java
+++ src/main/java/com/project/echoeco/project/ProjectController.java
@@ -2,8 +2,6 @@ package com.project.echoeco.project;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,27 +16,27 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/project")
 public class ProjectController {
 
-	private final ProjectService projectService;
+  private final ProjectService projectService;
 
-	// 리스트 전체 조회
-	@GetMapping("/list")
-	public String projectList(Model model) {
-		List<Project> projectList = projectService.getProjects();
-		model.addAttribute("projectList", projectList);
-		return "projectList";
-	}
+  // 리스트 전체 조회
+  @GetMapping("/list")
+  public String projectList(Model model) {
+    List<Project> projectList = projectService.getProjects();
+    model.addAttribute("projectList", projectList);
+    return "projectList";
+  }
 
-	// 프로젝트 생성 페이지
-	@GetMapping("/create")
-	public String createProject() {
-		return "projectForm";
-	}
+  // 프로젝트 생성 페이지
+  @GetMapping("/create")
+  public String createProject() {
+    return "projectForm";
+  }
 
-	// 프로젝트 저장
-	@PostMapping("/create")
-	public String saveProject(ProjectDto projectDto, MultipartFile imgFile) throws Exception {
-		projectService.newProject(projectDto, imgFile);
-		return "redirect:/project/list";
-	}
+  // 프로젝트 저장
+  @PostMapping("/create")
+  public String saveProject(ProjectDto projectDto, MultipartFile imgFile) throws Exception {
+    projectService.newProject(projectDto, imgFile);
+    return "redirect:/project/list";
+  }
 
 }
