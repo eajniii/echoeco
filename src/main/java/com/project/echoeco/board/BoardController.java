1b42e5f7925a15385227aee508fc66dc2eec2d12
jisu kim <yyjisu@gmail.com>
2023-09-11 11:13:32 +0900

    0911

--- src/main/java/com/project/echoeco/board/BoardController.java
+++ src/main/java/com/project/echoeco/board/BoardController.java
@@ -1,35 +1,6 @@
 package com.project.echoeco.board;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import groovy.util.logging.Slf4j;
-import lombok.RequiredArgsConstructor;
-
-@RestController
-@RequestMapping("/api/board")
-@RequiredArgsConstructor
-@Slf4j
-public class BoardController {
-
-	private final BoardService boardService;
-	@GetMapping("/list")
-	public String board() {
-		
-		return "board/list";
-	}
-	
-	@PostMapping("/")
-	public ResponseEntity<String> create(@RequestBody BoardDTO dto){
-		boardService.save(dto);
-		return ResponseEntity.ok().body("성공");
-	}
-	
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
