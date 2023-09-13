04da10d400aead19b9ce33b964283b9a96ca7252
jisu kim <yyjisu@gmail.com>
2023-09-13 20:19:38 +0900

    errorcode 수정

--- src/main/java/com/project/echoeco/common/exception/ErrorCode.java
+++ src/main/java/com/project/echoeco/common/exception/ErrorCode.java
@@ -10,7 +10,7 @@ import lombok.Getter;
 public enum ErrorCode {
 
   ID_NOT_FOUND(HttpStatus.NOT_FOUND,"존재하지 않는 ID입니다. 다시 시도해 주세요"),
-  DATA_NOT_FOUND(HttpStatus.NOT_FOUND,"요청하신 데이터가 존재하지 않습니다. 다시 시도해 주세요");
+  DATA_NOT_FOUND(HttpStatus.NOT_FOUND,"요청하신 데이터가 존재하지 않습니다. 다시 시도해 주세요"),
   MEMBER_DUPLICATED(HttpStatus.CONFLICT, "중복된 아이디가 존재합니다. 다시 시도해 주세요."),
   EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 이메일입니다. 다시 시도해 주세요."),
   INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "잘못된 패스워드입니다. 다시 시도해 주세요."),
