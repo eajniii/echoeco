bdc21de30942860138df08b4eedd68234d37f7fc
HWANG INJAE <sub.ij002@gmail.com>
2023-09-20 17:21:27 +0900

    로그인 보완 필요

--- src/frontend/src/pages/member/Login.jsx
+++ src/frontend/src/pages/member/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
     setIsLodeaing(true);
     auth.login(emailInput, passwordInput);
     setIsLodeaing(false);
-    if (auth.isWellDone) {
+    if (auth.isPostSuccess) {
       nevigate('/', { replace: true });
     }
   };
@@ -70,37 +70,3 @@ const Login = () => {
   );
 };
 export default Login;
-// import axios from 'axios';
-
-// const JWT_EXPIRY_TIME = 60 * 60 * 1000;
-
-// onLogin = (email, password) => {
-//   const data = {
-//     email,
-//     password
-//   };
-//   axios
-//     .post('/login', data)
-//     .then(response => {
-//       const { accessToken } = response.data;
-
-//       axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-//     })
-//     .catch(error => {});
-// };
-
-// onSilentRefresh = () => {
-//   axios
-//     .post('/silent-refresh', data)
-//     .then(onLoginSuccess)
-//     .catch(error => {});
-// };
-
-// onLoginSuccess = response => {
-//   const { accessToken } = response.data;
-//   // accessToken 설정
-//   axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-
-//   // accessToken 만료하기 1분 전에 로그인 연장
-//   setTimeout(onSilentRefresh, JWT_EXPIRRY_TIME - 60000);
-// };
